c9ee6e1a2aebabf0be0e191242eadfa69890a914
slim1616 <slim1616@users.noreply.github.com>
2021-08-27 20:13:10 +0100

    groupes ok

--- src/components/classementPhrase.vue
+++ src/components/classementPhrase.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    {{ flatWordsgroupesIds }}
     <div>
       <div>
         <button @click.prevent.stop="lang = 'ltr'">français</button>
@@ -18,17 +19,102 @@
 
     <div>
       <template v-for="element in words">
-        <span :key="element.id" :class="{}" class="span">
+        <span
+          :key="element.id"
+          :class="{ exist: flatWordsgroupesIds.includes(element.id) }"
+          class="span"
+          draggable
+          @dragstart="startDrag($event, element)"
+        >
           {{ element.word }}
         </span>
       </template>
     </div>
-    <div></div>
-
+    <div>
+      <input type="text" v-model="groupename" placeholder="Groupe" />
+      <button v-if="groupename.length >= 3" @click.prevent.stop="addGroupe">
+        Ajouter groupe
+      </button>
+    </div>
+    <div class="groupes">
+      <template v-if="groupes.length > 0">
+        <div
+          v-for="(groupe, i) in groupes"
+          :key="i"
+          class="groupe"
+          @dragover.prevent
+          @dragenter.prevent
+          @dragover="dragover"
+          @drop="onDrop($event, groupe)"
+        >
+          <div>
+            <button @click.prevent.stop="effaceGroupe(groupe)">X</button>
+            <h3>{{ groupe.groupename }}</h3>
+          </div>
+          <ul>
+            <template v-if="groupe.items.length > 0">
+              <li v-for="(item, i) in groupe.items" :key="i">
+                <button @click.prevent.stop="effaceItem(groupe, item)">
+                  X
+                </button>
+                {{ item.word }}
+              </li>
+            </template>
+          </ul>
+        </div>
+      </template>
+    </div>
     <hr />
     <p>élève</p>
-
     <div>
+      <button @click.prevent.stop="initialiser">Start</button>
+    </div>
+    <div>
+      <template v-for="element in words">
+        <span
+          :key="element.id"
+          :class="{ exist: flatWordsgroupesEleves.includes(element.id) }"
+          class="span"
+          draggable
+          @dragstart="startDragEleve($event, element)"
+        >
+          {{ element.word }}
+        </span>
+      </template>
+    </div>
+    <div>
+      <div class="groupes">
+        <template v-if="groupesEleves.length > 0">
+          <div
+            v-for="(groupe, i) in groupesEleves"
+            :key="i"
+            class="groupe"
+            @dragover.prevent
+            @dragenter.prevent
+            @dragover="dragover"
+            @drop="onDropEleve($event, groupe)"
+          >
+            <div>
+              <button @click.prevent.stop="effaceGroupe(groupe)">X</button>
+              <h3>{{ groupe.groupename }}</h3>
+            </div>
+            <ul>
+              <template v-if="groupe.items.length > 0">
+                <li
+                  v-for="(item, i) in groupe.items"
+                  :key="i"
+                  :class="{
+                    correct: isCorrect(groupe, item),
+                    wrong: !isCorrect(groupe, item),
+                  }"
+                >
+                  {{ item.word }}
+                </li>
+              </template>
+            </ul>
+          </div>
+        </template>
+      </div>
       <button @click="showCorrection = true">Corriger</button>
     </div>
   </div>
@@ -38,7 +124,9 @@
 export default {
   data() {
     return {
+      groupename: "",
       groupes: [],
+      groupesEleves: [],
       showCorrection: false,
       lang: "ltr",
       currentWord: null,
@@ -51,29 +139,77 @@ export default {
     };
   },
   methods: {
+    effaceItem(groupe, item) {
+      let gr = this.groupes.find((gr) => gr.groupename == groupe.groupename);
+      if (gr) {
+        gr.items = gr.items.filter((it) => it.id != item.id);
+      }
+    },
+    isCorrect(groupe, item) {
+      let gr = this.groupes.find((gr) => gr.groupename == groupe.groupename);
+      let exist = false;
+      if (gr) {
+        if (gr.items.length > 0) {
+          gr.items.forEach((it) => {
+            if (it.id == item.id) {
+              exist = true;
+            }
+          });
+        }
+      }
+      return exist;
+    },
+    initialiser() {
+      let arr = [];
+      this.groupes.forEach((gr) => {
+        arr.push({ groupename: gr.groupename, items: [] });
+      });
+      this.groupesEleves = arr;
+    },
+    addGroupe() {
+      this.groupes.push({ groupename: this.groupename, items: [] });
+      this.groupename = "";
+    },
+    effaceGroupe(groupe) {
+      this.groupes = this.groupes.filter(
+        (gr) => gr.groupename != groupe.groupename
+      );
+    },
+    startDragEleve: (evt, item) => {
+      console.log(item);
+      evt.dataTransfer.dropEffect = "move";
+      evt.dataTransfer.effectAllowed = "move";
+      evt.dataTransfer.setData("text", JSON.stringify(item));
+    },
+    onDropEleve(evt, groupe) {
+      console.log(evt.dataTransfer.getData("text"));
+      const newEl = JSON.parse(evt.dataTransfer.getData("text"));
+      let groupes = this.groupesEleves;
+      this.groupesEleves.forEach((gr) => {
+        if (gr.groupename == groupe.groupename) {
+          gr.items.push(newEl);
+        }
+      });
+      this.groupesEleves = groupes;
+      console.log(newEl);
+    },
     startDrag: (evt, item) => {
       console.log(item);
       evt.dataTransfer.dropEffect = "move";
       evt.dataTransfer.effectAllowed = "move";
       evt.dataTransfer.setData("text", JSON.stringify(item));
     },
-    onDrop(evt, word) {
+    onDrop(evt, groupe) {
       console.log(evt.dataTransfer.getData("text"));
       const newEl = JSON.parse(evt.dataTransfer.getData("text"));
-      console.log(newEl);
-      this.list2 = this.list2.map((mot) => {
-        if (mot.id == word.id) {
-          mot.reponse = newEl;
+      let groupes = this.groupes;
+      groupes.forEach((gr) => {
+        if (gr.groupename == groupe.groupename) {
+          gr.items.push(newEl);
         }
-        return mot;
       });
-      if (newEl.id == word.id) {
-        this.reponses.push(newEl);
-      }
-      console.log(word);
-      //const itemID = evt.dataTransfer.getData("itemID");
-      //const item = this.items.find((item) => item.id == itemID);
-      //item.list = list;
+      this.groupes = groupes;
+      console.log(newEl);
     },
     dragover(evt) {
       console.log("dragover");
@@ -134,10 +270,30 @@ export default {
     },
   },
   computed: {
-    elevesWords() {
-      return this.list2.map((word) => {
-        return { id: "r-" + word.id, word: word.word, liaison: word.liaison };
+    flatWordsgroupesIds() {
+      return this.flatWordsgroupes.map((el) => el.id);
+    },
+    flatWordsgroupes() {
+      let words = [];
+      this.groupes.forEach((gr) => {
+        if (gr.items.length > 0) {
+          gr.items.forEach((item) => {
+            words.push(item);
+          });
+        }
       });
+      return words;
+    },
+    flatWordsgroupesEleves() {
+      let words = [];
+      this.groupesEleves.forEach((gr) => {
+        if (gr.items.length > 0) {
+          gr.items.forEach((item) => {
+            words.push(item.id);
+          });
+        }
+      });
+      return words;
     },
   },
   mounted() {},
@@ -188,5 +344,20 @@ p {
 }
 .wrong {
   background: red;
+}
+.groupe {
+  background: #eee;
+  min-width: 150px;
+  max-width: 250px;
+  margin: 2px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.groupe h3 {
+  margin: 5px;
+}
+.groupes {
+  display: flex;
 }
 </style>
